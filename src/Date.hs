f53434ff1b53496b4c266e66dcde890ddea014e6
Christophe Thibaut <christophethibaut@macbook-pro-de-christophe.home>
2022-05-26 17:30:41 +0200

    Date type

---
+++ src/Date.hs
@@ -0,0 +1,19 @@
+module Date
+    where
+
+import Data.Time
+
+data Date = Date Day
+
+instance Show Date where
+    show (Date d) = show d
+
+instance Read Date where
+    readsPrec _ = \s -> case parse s :: Maybe Day of
+                          Nothing -> []
+                          Just d  -> [(Date d,"")]
+
+theDay :: Integer -> Int -> Int -> Date
+theDay y m d = Date (fromGregorian y m d)
+
+parse = parseTimeM True defaultTimeLocale "%Y-%m-%d"
---
+++ test/DateSpec.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+module DateSpec where
+
+import Test.Hspec
+import Date
+
+
+spec:: SpecWith ()
+spec = do
+    describe "a date" $ do
+        it "can be created from a year, month and day" $ do
+            let d = theDay 2022 05 26
+            show d `shouldBe` "2022-05-26"
+
+        it "can be read from a string" $ do
+            let d = read "2022-05-26" :: Date
+            show d `shouldBe` "2022-05-26"
+
